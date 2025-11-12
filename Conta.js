26ac01fe4b5dc6507f85e51b182bd2636bd55765
danielschulz07 <danielschulzdasilva@gmail.com>
2025-11-12 20:52:54 -0300

    Movendo os arquivos iniciais

---
+++ Conta.js
@@ -0,0 +1,70 @@
+import { Pessoa } from ".Pessoa.js";
+
+export class Conta {
+    static #qtdContas = 0;
+    #id;
+    #saldo;
+    #titular;
+
+    constructor(cliente, saldo = 0.0){
+        this.titular = cliente;
+
+        if(saldo < 0){
+            this.#saldo = 0;
+        } else {
+            this.#saldo = saldo;
+        }
+
+        Conta.#qtdContas++;
+        this.#id = ""+ new Date().getFullYear() + Conta.#qtdContas;
+    }
+
+    static get qtdContas(){
+        return Conta.#qtdContas;
+    }
+
+    set titular(cliente){
+        if(cliente == undefined || cliente instanceof Pessoa){
+            this.#titular = cliente;
+        }
+    }
+
+    get titular(){
+        return this.#titular
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    depositar(valor) {
+        if(valor > 0){
+            this.#saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    sacar(valor){
+        if(valor <= this.#saldo){
+            this.#saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    transferir(valor, contaDestino){
+        if(contaDestino instanceof Conta && this.sacar(valor)){
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    toString() {
+        return ("\nID: " + this.#id +
+        "\nSaldo: " + this.#saldo.toFixed(2) +
+        "\nTitular: " + this.#titular.toString());
+    }
+}
---
+++ ContaCorrente.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    sacar(valor){
+        if(valor <= (super.saldo + this.#limiteCredito - this.#saldoDevedor)){
+            if(!super.sacar(valor)){
+                valor -= super.saldo;
+                super.sacar(super.saldo)
+                this.#saldoDevedor =+ valor;
+            }
+            return true;
+        }
+        return false;
+    }
+        */
