265b4f96712b603c4e3f558c0e102148d7712edf
rNtop4444 <gaminganddeving@gmail.com>
2025-11-17 10:34:33 -0300

    adicionado o virames das contas

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ conta1.transferir(trans, conta1)
 */
 
 
-let contaCorrente1 = new ContaCorrente("DAdiel", 100, "22222222", undefined, undefined, undefined,)
+let contaCorrente1 = new ContaCorrente("DAdiel", 1, "22222222", undefined, undefined, undefined,)
 
 let contaPoup1 = new ContaPoupanca("bomdia", 200, undefined,0.5);
 
@@ -49,15 +49,17 @@ let cli2 = new Cliente("Zico", "654.321.987-11", "03/03/2003", 500, -500.0, "VIP
 
 let conta = new Conta("marcos",99999999999);
 
-let vetPessoas = [func, cli1, cli2, test, testpoupanca, conta/*conta1, conta2*/];
+let vetPessoas = [func, cli1, cli2, contaCorrente1, contaPoup1, conta/*conta1, conta2*/];
 
-contaPoup1.transferir(100,contaCorrente1);
+//contaPoup1.transferir(100,contaCorrente1);
 contaPoup1.viraMes();
 contaPoup1.viraMes();
-conta.transferir(1,contaCorrente1);
-contaCorrente1.transferir(202,contaCorrente1);
+//conta.transferir(1,contaCorrente1);
+//contaCorrente1.transferir(202,contaCorrente1);
 //test.viraMes();
-//alert(test.limiteDisponivel());
+contaCorrente1.sacar(100);
+//contaCorrente1.viraMes();
+alert(contaCorrente1.limiteDisponivel());
 
 
 console.log("\nNomes das Pessoas:\n");
@@ -74,3 +76,14 @@ var pessoa2 = new Pessoa("Daniel", "34239482374", "09/09/2009");
 
 var vetClientesBanco = [pessoa1, pessoa2];
 var vetContasBanco = [contaCorrente1, contaPoup1];
+
+vetContasBanco.forEach((conta)=>conta.viraMes());
+
+
+
+console.log("\nNomes das Pessoas:\n");
+vetPessoas.forEach( (objPessoa) => {
+    console.log(objPessoa.toString());
+    //objPessoa.categoria = "VIP";
+    //console.log(objPessoa.categoria + " - " + objPessoa.constructor.name);
+});
