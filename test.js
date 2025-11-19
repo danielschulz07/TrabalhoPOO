4e6e77a06b37f27cd7c202d135c3bab8b59275f5
Daniel Schulz da Silva <danielschulzdasilva@gmail.com>
2025-11-19 07:53:49 -0300

    Update test.js

--- test.js
+++ test.js
@@ -6,17 +6,6 @@ import {ContaPoupanca} from "./ContaPoupanca.js";
 import { Pessoa } from "./Pessoa.js";
 
 
-
-console.log("\nNomes das Pessoas:\n");
-for (let i = 0; i < vetContas.length; i++) {
-
-    const contas = vetContas[i];
-
-
-    console.log(contas)
-
-}
-
 var pessoa1 = new Pessoa("Renato", "12334567789","10/10/2010");
 var pessoa2 = new Pessoa("Daniel", "34239482374", "09/09/2009");
 
@@ -30,10 +19,9 @@ var vetContasBanco = [contaCorrente1, contaPoup1];
 vetContasBanco.forEach((conta)=>conta.viraMes());
 
 
-
 console.log("\nNomes das Pessoas:\n");
-vetContasBanco.forEach( (conta) => {
-    console.log(conta.toString());
+vetClientesBanco.forEach( (pessoa) => {
+    console.log(pessoa.toString());
     //objPessoa.categoria = "VIP";
     //console.log(objPessoa.categoria + " - " + objPessoa.constructor.name);
 });
