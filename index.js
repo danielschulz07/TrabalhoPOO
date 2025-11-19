4aaf93e9abbae1c551a6c945dbbd55194ad3c0f9
Daniel Schulz da Silva <danielschulzdasilva@gmail.com>
2025-11-19 09:11:39 -0300

    Foi feito a parte de filtro por nome, aonde o usuario digita o nome que ele quer ixibir

---
+++ index.js
@@ -0,0 +1,43 @@
+import {Funcionario} from "./Funcionario.js";
+import {Cliente} from "./Cliente.js";
+import {Conta} from "./Conta.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import {ContaPoupanca} from "./ContaPoupanca.js";
+import { Pessoa } from "./Pessoa.js";
+
+const inFiltrNome = document.getElementById("inFiltrNome");
+const btOk = document.getElementById("btOk");
+const outSaida = document.getElementById("outSaida");
+
+btOk.addEventListener('click', filtrarPorNome);
+
+var pessoa1 = new Pessoa("Renato", "12334567789","10/10/2010");
+var pessoa2 = new Pessoa("Daniel", "34239482374", "09/09/2009");
+var contaCorrente1 = new ContaCorrente("Daniel", 100, undefined, undefined, undefined, undefined);
+var cotaCorrente2 = new ContaCorrente("Archimedes", 2, undefined, undefined, undefined, undefined);
+var contaPoup1 = new ContaPoupanca("Renatovisk", 200, undefined, 0.5);
+var contaPoup2 = new ContaPoupanca("Heloisa", 100, undefined, 1.0);
+
+var vetClientesBanco = [pessoa1, pessoa2];
+var vetContasBanco = [contaCorrente1,cotaCorrente2, contaPoup1, contaPoup2];
+
+
+function filtrarPorNome(titular) {
+  const nomeFiltrado = inFiltrNome.value.toUpperCase();
+  const resultado = vetContasBanco.filter(vetContasBanco => vetContasBanco.titular.toUpperCase().includes(nomeFiltrado)).join("\n");
+
+  vetContasBanco.forEach((conta)=>conta.viraMes());
+  outSaida.innerHTML = resultado;
+}
+
+console.log("\nNomes das Pessoas:\n");
+vetClientesBanco.forEach( (pessoa) => {
+    console.log(pessoa.toString());
+    //objPessoa.categoria = "VIP";
+    //console.log(objPessoa.categoria + " - " + objPessoa.constructor.name);
+});
+
+console.log("\nContas Banco:\n");
+vetContasBanco.forEach( (conta) => {
+    console.log(conta.toString());
+});
